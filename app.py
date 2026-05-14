dac603960b1b5140099b4a4d7bda82a862b805d9
313twd <daegu.hs15@allip.kr>
2026-05-14 16:20:46 +0900

    Add Procfile and gunicorn for Render deployment

--- app.py
+++ app.py
@@ -241,6 +241,8 @@ def api_news(currency):
     news = fetch_news(currency)
     return jsonify({'news': news, 'currency': currency})
 
+init_db()
+
 if __name__ == '__main__':
-    init_db()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
